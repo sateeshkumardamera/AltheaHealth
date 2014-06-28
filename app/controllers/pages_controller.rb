884c746b71e14cd88e953f499e5ece80019da26c
root <root@dev3.altheahealth.com>
2014-06-28 17:08:55 +0000

    changes for AWS to file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def index
     if @settings.default_campaign && ((user_signed_in? && current_user.admin?) || @settings.default_campaign.published_flag)
-    	if current_user.present?
+  	if current_user.present?
         redirect_to campaign_home_url(@settings.default_campaign, :sr=>current_user.sr)
       else
         
